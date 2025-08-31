74406ff0e916968155b5d388c0b16f0f2179b3e2
Joshua Kyengo <jkyengo900@gmail.com>
2025-08-31 20:02:03 +0300

    Sunday4

--- config/initializers/devise_jwt.rb
+++ config/initializers/devise_jwt.rb
@@ -1,16 +1,25 @@
 Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.secret_key_base
+    
+    # JWT dispatch (when logging in)
     jwt.dispatch_requests = [
       ['POST', %r{^/admin/sessions$}]
     ]
+    
+    # JWT revocation (when logging out)
     jwt.revocation_requests = [
       ['DELETE', %r{^/admin/sessions$}]
     ]
-    jwt.expiration_time = 30.minutes.to_i
+    
+    # JWT authentication (for all admin routes)
     jwt.request_formats = {
       admin_user: [:json]
     }
+    
+    # Add this line to enable JWT authentication
+    jwt.dispatch_requests << ['GET', %r{^/admin/}]
+    
+    jwt.expiration_time = 30.minutes.to_i
   end
 end
-
